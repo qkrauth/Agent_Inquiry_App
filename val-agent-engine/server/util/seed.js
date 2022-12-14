15bb3f53a52012ddb84b5e621dedd025b46c5700
Quinten Krauth <quintenkrauth19@gmail.com>
2022-12-14 12:34:00 -0500

    agent data added

--- val-agent-engine/server/util/seed.js
+++ val-agent-engine/server/util/seed.js
@@ -1,10 +1,154 @@
 let agentData = [
     {
-        image: "../images/astra.jpg",
+        image: "",
         name: "Astra",
         born: "Ghana",
         role: "Controller",
         abilities: "Gravity Well, Nebula, Nova Pulse",
         ult: "Cosmic Divide"
     },
+    {
+        image: "",
+        name: "Breach",
+        born: "Sweden",
+        role: "Initiator",
+        abilities: "Aftershock, Fault Line, Flashpoint",
+        ult: "Rolling Thunder"
+    },
+    {
+        image: "",
+        name: "Brimstone",
+        born: "United States",
+        role: "Controller",
+        abilities: "Incendiary, Sky Smoke, Stim Beacon",
+        ult: "Orbital Strike" 
+    },
+    {
+        image: "",
+        name: "Chamber",
+        born: "France",
+        role: "Sentinel",
+        abilities: "Headhunter, Rendezvous, Trademark",
+        ult: "Tour De Force"
+    },
+    {
+        image: "",
+        name: "Cypher",
+        born: "Morocco",
+        role: "Sentinel",
+        abilities: "Cyber Cage, Spycam, Trapwire",
+        ult: "Neutral Theft"
+    },
+    {
+        image: "",
+        name: "Fade",
+        born: "Turkey",
+        role: "Initiator",
+        abilities: "Haunt, Prowler, Seize",
+        ult: "Nightfall"
+    },
+    {
+        image: "",
+        name: "Jett",
+        born: "South Korea",
+        role: "Duelist",
+        abilities: "Cloudburst, Updraft, Tailwind",
+        ult: "Blade Storm"
+    },
+    {
+        image: "",
+        name: "KAY/0",
+        born: "Alternate Timeline",
+        role: "Initiator",
+        abilities: "Flash Drive, Fragment, Zero Point",
+        ult: "Null"
+    },
+    {
+        image: "",
+        name: "Killjoy",
+        born: "Germany",
+        role: "Sentinel",
+        abilities: "Alarmbot, Nanoswarm, Turret",
+        ult: "Lockdown"
+    },
+    {
+        image: "",
+        name: "Neon",
+        born: "Philippines",
+        role: "Duelist",
+        abilities: "Fast Lane, High Gear, Relay Bolt",
+        ult: "Overdrive"
+    },
+    {
+        image: "",
+        name: "Omen",
+        born: "Memory",
+        role: "Controller",
+        abilities: "Dark Cover, Paranoia, Shrouded Step",
+        ult: "From The Shadows"
+    },
+    {
+        image: "",
+        name: "Phoenix",
+        born: "United Kingdom",
+        role: "Duelist",
+        abilities: "Blaze, Curveball, Hot Hands",
+        ult: "Run It Back"
+    },
+    {
+        image: "",
+        name: "Raze",
+        born: "Brazil",
+        role: "Duelist",
+        abilities: "Blast Pack, Boom Bot, Paint Shells",
+        ult: "Showstopper"
+    },
+    {
+        image: "",
+        name: "Reyna",
+        born: "Mexico",
+        role: "Duelist",
+        abilities: "Devour, Dismiss, Leer",
+        ult: "Empress"
+    },
+    {
+        image: "",
+        name: "Sage",
+        born: "China",
+        role: "Sentinel",
+        abilities: "Barrier Orb, Healing Orb, Slow Orb",
+        ult: "Resurrection"
+    },
+    {
+        image: "",
+        name: "Skye",
+        born: "Austrailia",
+        role: "Initiator",
+        abilities: "Guiding Light, Regrowth, Trailblazer",
+        ult: "Seekers"
+    },
+    {
+        image: "",
+        name: "Sova",
+        born: "Russia",
+        role: "Initiator",
+        abilities: "Owl Drone, Recon Bolt, Shock Bolt",
+        ult: "Hunters Fury"
+    },
+    {
+        image: "",
+        name: "Viper",
+        born: "United States",
+        role: "Controller",
+        abilities: "Poison Cloud, Snake Bite, Toxic Screen",
+        ult: "Vipers Pit"
+    },
+    {
+        image: "",
+        name: "Yoru",
+        born: "Japan",
+        role: "Duelist",
+        abilities: "Blindside, Fakeout, Gatecrash",
+        ult: "Dimensional Drift"
+    }
 ]
