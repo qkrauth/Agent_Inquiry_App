289d0d3be6dcd65e442ebe7b40ec46d6befb9aba
Quinten Krauth <quintenkrauth19@gmail.com>
2022-12-16 16:06:04 -0500

    data now on home screen

--- val-agent-engine/server/util/seed.js
+++ val-agent-engine/server/util/seed.js
@@ -1,6 +1,6 @@
 let agentData = [
     {
-        image: "../../src/images/astra.jpg",
+        image: "https://cdn1.dotesports.com/wp-content/uploads/2021/02/26192225/Astra_Wallpapers_Blue1-1-scaled.jpg",
         name: "Astra",
         born: "Ghana",
         role: "Controller",
@@ -8,7 +8,7 @@ let agentData = [
         ult: "Cosmic Divide"
     },
     {
-        image: "",
+        image: "https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2020/10/30/odxgos7ooyc39zura8wn/valorant-operator-breach",
         name: "Breach",
         born: "Sweden",
         role: "Initiator",
@@ -16,7 +16,7 @@ let agentData = [
         ult: "Rolling Thunder"
     },
     {
-        image: "",
+        image: "https://gamingbolt.com/wp-content/uploads/2020/07/Valorant_Brimstone.jpg",
         name: "Brimstone",
         born: "United States",
         role: "Controller",
@@ -24,7 +24,7 @@ let agentData = [
         ult: "Orbital Strike" 
     },
     {
-        image: "",
+        image: "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8e68c6e841ded930/6181a5e29b08490cdd119059/VALORANT_Ep3_A3_Social_Updates_ChamberContent_Stack_Thumbnail_CHAMBER.jpg",
         name: "Chamber",
         born: "France",
         role: "Sentinel",
@@ -32,7 +32,7 @@ let agentData = [
         ult: "Tour De Force"
     },
     {
-        image: "",
+        image: "https://s3-eu-central-1.amazonaws.com/www-staging.esports.com/wp-media-folder-esports-com/app/uploads/2022/03/Cypher-VALORANT-removed-temporarily.jpg",
         name: "Cypher",
         born: "Morocco",
         role: "Sentinel",
@@ -40,7 +40,7 @@ let agentData = [
         ult: "Neutral Theft"
     },
     {
-        image: "",
+        image: "https://s.yimg.com/os/creatr-uploaded-images/2022-04/31de8330-c41b-11ec-be7a-21208ad9bbd6",
         name: "Fade",
         born: "Turkey",
         role: "Initiator",
@@ -48,7 +48,7 @@ let agentData = [
         ult: "Nightfall"
     },
     {
-        image: "",
+        image: "https://cdn-wp.thesportsrush.com/2021/10/861b367a-valorant_jett_dark-scaled-1.jpg",
         name: "Jett",
         born: "South Korea",
         role: "Duelist",
@@ -56,7 +56,7 @@ let agentData = [
         ult: "Blade Storm"
     },
     {
-        image: "",
+        image: "https://s.yimg.com/os/creatr-uploaded-images/2021-06/420ce0e0-cf47-11eb-bdff-08e4ec3d7dac",
         name: "KAY/0",
         born: "Alternate Timeline",
         role: "Initiator",
@@ -64,7 +64,7 @@ let agentData = [
         ult: "Null"
     },
     {
-        image: "",
+        image: "https://cdn1.dotesports.com/wp-content/uploads/2021/06/23052003/KillJoy_Wallpapers_blue2-scaled-1.jpg",
         name: "Killjoy",
         born: "Germany",
         role: "Sentinel",
@@ -72,7 +72,7 @@ let agentData = [
         ult: "Lockdown"
     },
     {
-        image: "",
+        image: "https://www.esports.net/wp-content/uploads/2022/07/valorant-neon-main.png",
         name: "Neon",
         born: "Philippines",
         role: "Duelist",
@@ -80,7 +80,7 @@ let agentData = [
         ult: "Overdrive"
     },
     {
-        image: "",
+        image: "https://cdn.oneesports.gg/cdn-data/wp-content/uploads/2020/06/Valorant_Omen.jpg",
         name: "Omen",
         born: "Memory",
         role: "Controller",
@@ -88,7 +88,7 @@ let agentData = [
         ult: "From The Shadows"
     },
     {
-        image: "",
+        image: "https://cdn1.dotesports.com/wp-content/uploads/2021/01/27172556/VALORANT_Phoenix_Dark-2048x1152-1.jpg",
         name: "Phoenix",
         born: "United Kingdom",
         role: "Duelist",
@@ -96,7 +96,7 @@ let agentData = [
         ult: "Run It Back"
     },
     {
-        image: "",
+        image: "https://cdn.oneesports.gg/cdn-data/wp-content/uploads/2020/06/Valorant_Raze.jpg",
         name: "Raze",
         born: "Brazil",
         role: "Duelist",
@@ -104,7 +104,7 @@ let agentData = [
         ult: "Showstopper"
     },
     {
-        image: "",
+        image: "https://cdn.mos.cms.futurecdn.net/sW9nNjXjgVqSm7dx2SiLim.jpg",
         name: "Reyna",
         born: "Mexico",
         role: "Duelist",
@@ -112,7 +112,7 @@ let agentData = [
         ult: "Empress"
     },
     {
-        image: "",
+        image: "https://cdn1.dotesports.com/wp-content/uploads/2020/03/26102924/Sage_Char.Art_VALORANT.jpg",
         name: "Sage",
         born: "China",
         role: "Sentinel",
@@ -120,7 +120,7 @@ let agentData = [
         ult: "Resurrection"
     },
     {
-        image: "",
+        image: "https://cdn1.dotesports.com/wp-content/uploads/2021/06/23052658/Skye_KeyArt_PR_2-scaled-1-1.jpg",
         name: "Skye",
         born: "Austrailia",
         role: "Initiator",
@@ -128,7 +128,7 @@ let agentData = [
         ult: "Seekers"
     },
     {
-        image: "",
+        image: "https://img.redbull.com/images/c_crop,w_3840,h_1920,x_0,y_0,f_auto,q_auto/c_scale,w_1200/redbullcom/2021/4/14/lyuguphp3zyhcxdvyfat/valorant-sova",
         name: "Sova",
         born: "Russia",
         role: "Initiator",
@@ -136,7 +136,7 @@ let agentData = [
         ult: "Hunters Fury"
     },
     {
-        image: "",
+        image: "https://www.talkesport.com/wp-content/uploads/valorant-viper.jpg",
         name: "Viper",
         born: "United States",
         role: "Controller",
@@ -144,7 +144,7 @@ let agentData = [
         ult: "Vipers Pit"
     },
     {
-        image: "",
+        image: "https://preview.redd.it/2qt7xdh061b61.png?width=3840&format=png&auto=webp&s=ece0ddfce842b27b856093607fef877111aa4262",
         name: "Yoru",
         born: "Japan",
         role: "Duelist",
--- val-agent-engine/src/components/Card.jsx
+++ val-agent-engine/src/components/Card.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-const Card = ({player}) => {
-    return (
-        <div>
-            <h3>{player.name}</h3>
-            <h3>{player.born}</h3>
-            <h3>{player.role}</h3>
-            <h3>{player.abilities}</h3>
-            <h3>{player.ult}</h3>
-            <img src={player.image} />
-
-        </div>
-    );
-}
+const Card = ({ player }) => {
+  return (
+    <div className="agent-card">
+      <div className="agent-image">
+        <img src={player.image} alt=""/>
+      </div>
+      <h3>Name: {player.name}</h3>
+      <h3>Born: {player.born}</h3>
+      <h3>Role: {player.role}</h3>
+      <h3>Abilities: {player.abilities}</h3>
+      <h3>Ult: {player.ult}</h3>
+      <div>
+        <button>Add To Team</button>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
