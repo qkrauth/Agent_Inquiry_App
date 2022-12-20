a014bb3b922bdc534570ffa83d3f0a0b6d7b53a6
Quinten Krauth <quintenkrauth19@gmail.com>
2022-12-20 16:44:26 -0500

    login & register screen added

--- val-agent-engine/src/screens/LoginScreen.jsx
+++ val-agent-engine/src/screens/LoginScreen.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 const LoginScreen = () => {
-    return (
-        <div>LoginScreen</div>
-    );
+  return (
+    <div className="login-area">
+      <form>
+        <h2>Log in</h2>
+        <input type="text" placeholder="username:" />
+        <input type="text" placeholder="password:" />
+        <button>Sign in</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginScreen;
--- val-agent-engine/src/screens/RegisterScreen.jsx
+++ val-agent-engine/src/screens/RegisterScreen.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 const RegisterScreen = () => {
     return (
-        <div>RegisterScreen</div>
+        <div className="register-area">
+            <form>
+                <h2>Register</h2>
+                <input type="text" placeholder="username:"/>
+                <input type="text" placeholder="password:"/>
+                <button>Sign up</button>
+            </form>
+        </div>
     );
 };
 
